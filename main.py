2445660d42aef592426fa901420d0b51dae0259e
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-04-06 22:02:45 -0300

    add condition to check if city name is empty and updated print

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import requests
-api_key = 'your_api_key'
-city = 'São Paulo'
-API_URL = (f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}')
-response = requests.get(API_URL)
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-print(response)
-if response.status_code == 200:
-    dados = response.json()
-    temperature = dados['main']['temp']
-    min_temp = dados['main']['temp_min']
-    max_temp = dados['main']['temp_max']
-    humidity = dados['main']['humidity']
-    winds = dados['wind']['speed']
-    print(f'Temperature:{temperature} Min Temperature:{min_temp} Max Temp:{max_temp} Humidity:{humidity} Winds:{winds}')
+api_key = os.getenv('API_KEY') # Edit your API_KEY variable in .env file
+city = input('Which City do you want to know the Weather? ')
+
+if not city: # Condition check if city string is empty
+    print('You need to type a City!')
 else:
-    print(f'Erro: {response.status_code}')
-
-
+    API_URL = (f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}')
+    response = requests.get(API_URL)
+    print(response)
+    if response.status_code == 200:
+        dados = response.json()
+        temperature = dados['main']['temp']
+        min_temp = dados['main']['temp_min']
+        max_temp = dados['main']['temp_max']
+        humidity = dados['main']['humidity']
+        winds = dados['wind']['speed']
+        print(f'Temperature: {temperature:.1f}ºC \n Min. Temperature: {min_temp:.1f}ºC \n Max. Temperature: {max_temp:.1f}ºC \n Humidity: {humidity}% \n Winds: {winds} m/s')
+    else:
+        print(f'Erro: {response.status_code}')
