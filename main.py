c15f04fde35bebb4ad32a40b02b7886ccf311f74
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-04-07 23:22:50 -0300

    refactor: add function to call city name on main.py

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.getenv('API_KEY') # Edit your API_KEY variable in .env file
-city = input('Which City do you want to know the Weather? ')
 
-if not city: # Condition check if city string is empty
-    print('You need to type a City!')
-else:
+def get_weather_data(city):
     API_URL = (f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}')
     response = requests.get(API_URL)
     print(response)
@@ -19,6 +16,13 @@ else:
         max_temp = dados['main']['temp_max']
         humidity = dados['main']['humidity']
         winds = dados['wind']['speed']
-        print(f'Temperature: {temperature:.1f}ºC \n Min. Temperature: {min_temp:.1f}ºC \n Max. Temperature: {max_temp:.1f}ºC \n Humidity: {humidity}% \n Winds: {winds} m/s')
+        return {
+            'temperature': temperature,
+            'min_temp': min_temp,
+            'max_temp': max_temp,
+            'humidity': humidity,
+            'winds': winds
+        }
     else:
-        print(f'Erro: {response.status_code}')
+        return None
+    
