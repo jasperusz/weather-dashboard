0f08c413ddfd9e7db0157a15193d9376d47a241b
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-04-07 23:28:11 -0300

    feat: app.py now receives city from main.py and shows weather

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from main import get_weather_data
 
 app = Flask(__name__)
 
@@ -11,7 +12,8 @@ def index():
 @app.get('/weather')
 def get_weather():
     city_name = request.args.get('city_name')
-    return f'Weather Conditions from City {city_name}'
+    city_weather = get_weather_data(city_name)
+    return f'Weather Conditions from City {city_name}: {city_weather}'
 
 if __name__ == '__main__':
     app.run(debug=True)
