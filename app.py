d39adf9c1849e1d960728f0979997eb378619100
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-04-25 22:46:30 -0300

    feat: restrict weather search to authenticated users via Google Oauth

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, Column, String, Float, DateTime
 from datetime import datetime
 from main import get_weather_data
+from authlib.integrations.flask_client import OAuth
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -14,8 +18,22 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///weather.db" # SQLite database and location
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 
 db.init_app(app)
+
+# OAuth Configuration
+
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=os.getenv('GOOGLE_CLIENT_ID'),
+    client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
 
 # Class to create a table in db to store search history from users
 class Search(Base):
@@ -29,7 +47,29 @@ class Search(Base):
 # Create route to index.html through app.route
 @app.route('/')
 def index():
-    return render_template('index.html', title="Home Page")
+    user=session.get('user')
+    return render_template('index.html', title="Home Page", user=user)
+
+# Create route to login
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth_callback', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+# Create route to logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+# Create route to callback after login
+@app.route('/auth/callback')
+def auth_callback():
+    token = google.authorize_access_token()
+    user_info = token.get('userinfo') 
+    session['user'] = user_info
+    session['email'] = user_info['email']
+    return redirect(url_for('index'))
 
 # Use GET method shortcut "app.get" to get /weather from index form
 @app.get('/weather')
@@ -42,7 +82,7 @@ def get_weather():
         search = Search(city=city_name, temperature=city_weather['temperature'], searched_at=current_time)
         db.session.add(search)
         db.session.commit()
-    return render_template('index.html', city_name=city_name, city_weather=city_weather)
+    return render_template('index.html', city_name=city_name, city_weather=city_weather, user=session.get('user'))
 
 with app.app_context():
     db.create_all()
--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,25 @@
+const currentTheme = localStorage.getItem('theme') || 'light';
+
+if (currentTheme) {
+    nightMode(currentTheme);
+}
+
 function nightMode(mode){
     const body = document.body;
     const btn = document.getElementById('night-mode-button');
     
     if (mode === 'night'){
+        theme = 'night';
         body.classList.add('night-theme');
         btn.src = btn.src.replace('moon-32.png', 'light-32.png');
+        localStorage.setItem('theme', 'night');
         btn.onclick = function() { nightMode('light') };
 
     } else if (mode === 'light'){
+        theme = 'light'
         body.classList.remove('night-theme');
         btn.src = btn.src.replace('light-32.png', 'moon-32.png');
+        localStorage.setItem('theme', 'light');
         btn.onclick = function() { nightMode('night') };
     }
 };
